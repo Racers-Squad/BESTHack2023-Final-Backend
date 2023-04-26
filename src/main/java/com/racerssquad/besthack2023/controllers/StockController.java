ab716b794807a97439c73cfcef4a948886803954
AgeDim <proga778@gmail.com>
2023-04-26 15:37:27 +0300

    Created stub for graphics

---
+++ src/main/java/com/racerssquad/besthack2023/controllers/StockController.java
@@ -0,0 +1,43 @@
+package com.racerssquad.besthack2023.controllers;
+
+import com.racerssquad.besthack2023.DTO.StockRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class StockController {
+
+    ArrayList<StockRequest> services = new ArrayList<>() {{
+        add(new StockRequest("QWD", "asda", "171634", "1", "124124412", "10-10-2010"));
+        add(new StockRequest("ASD", "afgjhfj", "134576", "2", "1412421", "10-10-2010"));
+        add(new StockRequest("ZXC", "awdfaw", "352", "3", "345345", "10-10-2010"));
+        add(new StockRequest("XYZ", "xui", "1337", "0", "123123123", "10-10-2010"));
+        add(new StockRequest("TUYKJ", "wertwet", "", "5", "23576457", "10-10-2010"));
+        add(new StockRequest("HYJ", "ului", "358", "6", "32456743", "10-10-2010"));
+        add(new StockRequest("ADF", "erty", "15324", "7", "21512", "10-10-2010"));
+        add(new StockRequest("GHJK", "xbvx", "38795", "8", "4278478", "10-10-2010"));
+        add(new StockRequest("AWF", "yilpo", "1253", "9", "2457245", "10-10-2010"));
+        add(new StockRequest("Forex", "stjr", "673145", "10", "1346134", "10-10-2010"));
+    }};
+
+    @GetMapping("/stocks/{name}")
+    public ResponseEntity<?> getStocks(@PathVariable String name) {
+        return ResponseEntity.ok(services);
+    }
+
+    @GetMapping("/stock/{code}")
+    public ResponseEntity<?> getStockByCode(@PathVariable String code) {
+        for (StockRequest stock : services) {
+            if (stock.getCode().equals(code)) {
+                return ResponseEntity.ok(stock);
+            }
+        }
+        return ResponseEntity.badRequest().body("No such element in array!");
+    }
+}
--- src/main/java/com/racerssquad/besthack2023/controllers/WebSocketController.java
+++ src/main/java/com/racerssquad/besthack2023/controllers/WebSocketController.java
@@ -1,11 +1,17 @@
 package com.racerssquad.besthack2023.controllers;
 
+import com.racerssquad.besthack2023.DTO.ChartRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
 
 @Controller
 public class WebSocketController {
@@ -16,13 +22,20 @@ public class WebSocketController {
         this.messagingTemplate = messagingTemplate;
     }
 
-    @MessageMapping("/auth")
-    @SendTo("/topic/auth")
+    @MessageMapping("/stock")
+    @SendTo("/topic/stock")
     @CrossOrigin("*")
-    public String handlePing(String message) {
-        System.out.println("Received message: " + message);
-        String response = "Pong " + System.currentTimeMillis();
-        messagingTemplate.convertAndSend("/topic/auth", response);
-        return response;
+    public ArrayList<Number> handlePrice(ChartRequest stock) {
+        System.out.println("Received message: " + stock.toString());
+        ArrayList<Number> response = new ArrayList<>();
+        if (stock.getChartType().equals("1")) {
+            response = new ArrayList<>(Arrays.asList(1200, 1783, 1631, 2214, 2062, 2645, 2493, 3076,
+                    2924, 3507, 3355, 3938, 3786, 4369, 4217, 4800, 4648, 5231, 5079, 5662, 5510, 6093, 5941, 6524, 6372,
+                    6955, 6803, 7386, 7234, 7817, 7665, 8248, 8096, 8679, 8527, 9110, 8958, 9541, 9389, 9972, 9820, 10403,
+                    10251, 10834, 10682, 11265, 11113, 11696, 11544, 12127, 11975, 12558, 12406, 12989, 12837, 13420, 13268,
+                    13851, 13699, 14282));
+        }
+            messagingTemplate.convertAndSend("/topic/price", response);
+            return response;
+        }
     }
-}
