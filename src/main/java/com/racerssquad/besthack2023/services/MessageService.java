89f3d7a3e3b786d811f6ce85e742519c7727baf7
1MikhailStepanov1 <wakamail923@gmail.com>
2023-04-29 23:22:35 +0300

    lil changes

--- src/main/java/com/racerssquad/besthack2023/services/MessageService.java
+++ src/main/java/com/racerssquad/besthack2023/services/MessageService.java
@@ -6,19 +6,12 @@ import com.racerssquad.besthack2023.util.MessageToExchangeInfoDeserializer;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
 
 @Service("mes_service")
 public class MessageService {
-
-    public byte[] processMessage(byte[] message){
-        String messageContent = new String(message);
-        log.trace("Received message: " + messageContent);
-        String responseContent = String.format("Message \"%s\" is processed", messageContent);
-        return responseContent.getBytes();
     public ExchangeInfoMessage processMessage(Message<ExchangeInfoMessage> msg) {
-        System.out.println("Received message:" + msg.getPayload().getRequest().getCommand().name());
+        log.trace("Received message:" + msg.getPayload().getRequest().getCommand().name());
         return msg.getPayload();
     }
 }
