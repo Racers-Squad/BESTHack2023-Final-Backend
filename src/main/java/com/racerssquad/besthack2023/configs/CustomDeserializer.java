cbe3e20408429e43cf405319b2e3b8702d0f08be
Matvey Chukhno <mr.chucno@yandex.ru>
2023-04-29 20:09:03 +0300

    Add deserializer

---
+++ src/main/java/com/racerssquad/besthack2023/configs/CustomDeserializer.java
@@ -0,0 +1,24 @@
+package com.racerssquad.besthack2023.configs;
+
+import org.springframework.core.serializer.Deserializer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class CustomDeserializer implements Deserializer<Message<String>> {
+
+    @Override
+    public Message<String> deserializeFromByteArray(byte[] serialized) throws IOException {
+        String payload = new String(serialized, StandardCharsets.UTF_8);
+        return MessageBuilder.withPayload(payload).build();
+    }
+
+    @Override
+    public Message<String> deserialize(InputStream inputStream) throws IOException {
+        String payload = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        return MessageBuilder.withPayload(payload).build();
+    }
+}
